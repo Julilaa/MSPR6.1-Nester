bdea2bc89d965b50d069a5c78b3ca219339123a4
Julilaa <juliem.ssc@gmail.com>
2024-03-04 23:37:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,5 +17,7 @@ def get_scans():
     #return jsonify(scans), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
